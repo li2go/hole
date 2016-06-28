abf0c2e125b0d9b646a680025c5c4fa4720b0f33
li2go <1055998753@qq.com>
2016-06-28 12:10:09 +0800

    auto set localServer IPAddress in local net

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
-
+function getIPAdress() {
+  var interfaces = require('os').networkInterfaces();
+  for (var devName in interfaces) {
+    var iface = interfaces[devName];
+    for (var i = 0; i < iface.length; i++) {
+      var alias = iface[i];
+      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+        return alias.address;
+      }
+    }
+  }
+}
+var localIPAddress=getIPAdress();
 new WebpackDevServer(webpack(config), {
     publicPath: config.output.publicPath,
     hot: true,
     historyApiFallback: true,
     stats: {colors: true},
 
-}).listen(3003, 'localhost', function (err, result) {
+}).listen(10086, localIPAddress, function (err, result) {
         if (err) {
             console.log(err);
         }
 
-        console.log('Listening at localhost:3003');
+        console.log('Listening at '+localIPAddress+':10086');
     });
